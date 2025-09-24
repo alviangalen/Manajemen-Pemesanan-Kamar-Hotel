e35a01e90e4a248d45b71bd55455012364e4c5ac
alviangalen <alviangalen@gmail.com>
2025-09-24 07:20:45 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Manajemen_Pemesanan_Kamar_Hotel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
