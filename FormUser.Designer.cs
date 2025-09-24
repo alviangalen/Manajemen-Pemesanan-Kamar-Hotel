29863d6326684644f8e220a8175eaa52d03cd4fe
atayarizi <atayaabimanyu8@gmail.com>
2025-09-24 07:34:09 +0700

    kanjur

---
+++ FormUser.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Manajemen_Pemesanan_Kamar_Hotel
+{
+    partial class FormUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(2, 23);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(300, 150);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // FormUser
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1120, 621);
+            Controls.Add(groupBox1);
+            Name = "FormUser";
+            Text = "FormUser";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+    }
+}
---
+++ FormUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manajemen_Pemesanan_Kamar_Hotel
+{
+    public partial class FormUser : Form
+    {
+        public FormUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
